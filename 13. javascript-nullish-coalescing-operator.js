9106518136c828bdd22d4fe058a0fb4f671f2787
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-17 01:21:53 +0600

    Nullish Coalescing Operator

---
+++ 13. javascript-nullish-coalescing-operator.js
@@ -0,0 +1,10 @@
+// Nullish Coalescing Operator
+// Nullish: null & undifined
+// Coalescing:  null + undifined
+
+let lang = false;
+console.log(lang ?? "JavaScript");
+console.log(lang || "JavaScript");
+console.log(lang && "JavaScript");
+
+// Short Circuit Conditions
