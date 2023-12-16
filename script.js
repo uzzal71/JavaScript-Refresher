f2f803ecfce45ecefa7c8cb5fbf34ca51bca3dad
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-17 00:26:52 +0600

    Object ticks

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ const language = {
     name: "JavaScript",
     year: 1995,
     creator: "Brendan Eich",
-}
+};
+
+let keyArray = Object.keys(language);
+console.log(keyArray);
+
+let valueArray = Object.values(language);
+console.log(valueArray);
+
+let entriesArray = Object.entries(language);
+console.log(entriesArray);
