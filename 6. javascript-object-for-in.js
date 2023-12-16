574c2a0488107e077f0ce75d8b2b052fadb9170c
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-16 14:12:03 +0600

    Object for in

--- 6. javascript-object-for-in.js
+++ 6. javascript-object-for-in.js
@@ -1,6 +1,10 @@
-// object tricks
+// object
 const language = {
     name: "JavaScript",
     year: 1995,
     creator: "Brendan Eich",
+}
+
+for (key in language) {
+    console.log(language[key]);
 }
