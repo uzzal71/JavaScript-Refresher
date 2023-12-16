6eb08eea1f81d3593b809bbb39db5c185eac7c7b
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-16 13:03:37 +0600

    javascript event handler

---
+++ 3. javascript-event-handler.js
@@ -0,0 +1,9 @@
+// sellect button
+const button = document.getElementById("button");
+
+// event handler
+function hello() {
+    console.log("Hello world");
+}
+
+button.addEventListener("click", hello);
