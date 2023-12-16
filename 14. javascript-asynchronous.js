cc7c12c63115e9c9b03f48ee3e4216b1f6881dcf
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-17 01:41:05 +0600

    Asynchronous

---
+++ 14. javascript-asynchronous.js
@@ -0,0 +1,8 @@
+// Asynchronous js
+async function getData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const data = await response.json();
+    console.log(data);
+}
+
+getData();
--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // Asynchronous js
 async function getData() {
-    const result = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const data = await response.json();
+    console.log(data);
 }
+
+getData();
