851f6f351a5b4171553182a84bc1421175b28861
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-16 12:40:14 +0600

    javascript function

---
+++ javascript-function.js
@@ -0,0 +1,60 @@
+/**
+ * JavaScript regular function by default return undefine, when you didn't return anythings 
+ */
+
+// regular function
+function hello() {
+    console.log("Hello world!");
+}
+
+let msg = hello();
+console.log(msg);
+
+// function expression
+const hello = function() {
+    console.log("Hello world");
+}
+
+// named function exprepression
+const hello = function hello() {
+    console.log("Hello world");
+}
+
+// arrow function
+const hello = () => {
+    console.log("Hello world");
+}
+
+const hello = () => 5;
+
+const hello = (a, b) => a + b;
+
+const hello = (a, b) => {
+    a++;
+    return a * b;
+}
+
+const hello = (a, b) => {
+    return {
+        a: 5,
+        b: 6
+    }
+}
+
+const hello = (a, b) => ({
+    a: 5,
+    b: 6
+});
+
+// anonymous function
+function hello() {
+    return () => {
+        console.log("Hello world");
+    }
+}
+
+function hello() {
+    return function(){
+        console.log("Hello world");
+    }
+}
